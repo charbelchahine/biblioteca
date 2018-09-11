91b018a9639a43b022089f9ba0c28afb4a4ad696
Nomaan Ahmed <nomaan.ahmed.8@gmail.com>
2018-09-10 22:50:21 -0400

    some sample code that everyone should commit their own version of on their own branches.

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+helloWorld = "Hello, world. Welcome to Biblioteca. My creators are:"
+nomaan = "Nomaan"
 
 def index(request):
-    return HttpResponse("Hello, world. Welcome to Biblioteca. --Nomaan")
+    return HttpResponse(helloWorld + " <br> " 
+    + nomaan)
