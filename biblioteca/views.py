2d04a1085b752745887c158bbdff3e3a7e1b5396
Nomaan Ahmed <nomaan.ahmed.8@gmail.com>
2018-09-19 23:28:00 -0400

    Created index page. settings.py must be modified for templates.

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-helloWorld = "Hello, world. Welcome to Biblioteca. My creators are:"
-nomaan = "Nomaan"
-
 def index(request):
-    return HttpResponse(helloWorld + " <br> " 
-    + nomaan)
+    return render(request, 'biblioteca/index.html')
--- soen343_project/urls.py
+++ soen343_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('biblioteca/', include('biblioteca.urls')),
+    path('', include('biblioteca.urls')),
 ]
