092d66ecdff1da8b0cb10e7abff47ff5be5aa9d0
mwPlouffe <mathew.plouffe@mail.mcgill.ca>
2018-10-10 19:59:29 -0400

    Changes to be committed: 	modified:   biblioteca/gateways.py
    Reverted Change suggested, due to it introducing a bug

--- biblioteca/gateways.py
+++ biblioteca/gateways.py
@@ -130,7 +130,8 @@ def unique_email(email):
     with connection.cursor() as c:
         c.execute('SELECT COUNT(email) FROM users WHERE email = %s', [email])
         data = c.fetchone()
-    return len(data) == 0
+    return int(data[0]) == 0
+
 def get_vtk_log():
     print('---------------')
     with connection.cursor() as cursor:
