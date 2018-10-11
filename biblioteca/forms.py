d9bd847e21a1c3dfc2aa71017e6d73a3a5fd2b96
steak-house <valp.perrot@gmail.com>
2018-10-11 17:00:34 -0400

    Issue #27 : changed username to email on login page

--- biblioteca/forms.py
+++ biblioteca/forms.py
@@ -2,7 +2,7 @@ from django import forms
 import datetime
 
 class LoginForm(forms.Form):
-	username = forms.CharField(label='Username', max_length=100)
+	username = forms.CharField(label='Email', max_length=100)
 	password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput())
 
 class RegisterForm(forms.Form):
